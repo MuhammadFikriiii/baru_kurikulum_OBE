9b4fdda5863be6b63a72340bfad44e2a7b3f22b5
MuhammadFikriiii <tpublicml@gmail.com>
2025-03-24 14:45:15 +0800

    benerin gambar

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -10,42 +10,43 @@
 <body>
   
   <div class="min-h-screen py-40" style="background-image: linear-gradient(115deg, #e7c9ae, #ebeae6)">
-    <div class="container mx-auto">
-      <div class="w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
-        <div class="w-1/2" style="background-image: url('image/Poliban.jpeg');">
-          <h1 class="text-white">Selamat Datang</h1>
-          <div>
-            <p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-          </div>
+    <div class="min-h-screen py-40 flex items-center justify-center" style="background-image: linear-gradient(115deg, #e7c9ae, #ebeae6)">
+      <div class="w-8/12 bg-white rounded-xl shadow-lg overflow-hidden flex flex-row-reverse">
+        
+        <!-- Bagian Gambar -->
+        <div class="w-1/2 bg-cover bg-center" style="background-image: url('/storage/image/poliban.jpeg');">
         </div>
+    
+        <!-- Bagian Form -->
         <div class="w-1/2 py-16 px-12">
-            <h2 class="text-3x1 mb-4">Sign-Up</h2>
-            <p class="mb-4">
-              Create your account.
-            </p>
-            <form action="#">
-              <div class="grid grid-cols-2 gap-5">
-                <input type="text" placeholder="First Name" class="border border-gray-400 py-1 px-2">
-                <input type="text" placeholder="Last Name" class="border border-gray-400 py-1 px-2">
-              </div>
-              <div class="mt-5">
-                <input type="text" placeholder="Email" class="border border-gray-400 py-1 px-2 w-full">
-                <input type="password" placeholder="Password" class="border border-gray-400 py-1 px-2 w-full">
-                <input type="password" placeholder="Confrim Password" class="border border-gray-400 py-1 px-2 w-full">
-              </div>
-              <div class="mt-5">
-                <input type="checkbox" class="border border-gray-400">
-                <span>
-                  Saya menerima Ketentuan <a href="#" class="text-orange-300 font-semibold">Penggunaan</a> & <a href="#"  class="text-orange-300 font-semibold">Kebijakan Privasi</a>
-                </span>
-              </div>
-              <div class="mt-5">
-                  <button class="w-full bg-orange-400 text-white" >Sign-Up</button>
-              </div>
-            </form>
+          <h2 class="text-3xl mb-4">Sign-Up</h2>
+          <p class="mb-4">Create your account.</p>
+          <form action="#">
+            <div class="grid grid-cols-2 gap-5">
+              <input type="text" placeholder="First Name" class="border border-gray-400 py-1 px-2">
+              <input type="text" placeholder="Last Name" class="border border-gray-400 py-1 px-2">
+            </div>
+            <div class="mt-5">
+              <input type="text" placeholder="Email" class="border border-gray-400 py-1 px-2 w-full">
+              <input type="password" placeholder="Password" class="border border-gray-400 py-1 px-2 w-full">
+              <input type="password" placeholder="Confirm Password" class="border border-gray-400 py-1 px-2 w-full">
+            </div>
+            <div class="mt-5">
+              <input type="checkbox" class="border border-gray-400">
+              <span>
+                Saya menerima Ketentuan <a href="#" class="text-orange-300 font-semibold">Penggunaan</a> & 
+                <a href="#" class="text-orange-300 font-semibold">Kebijakan Privasi</a>
+              </span>
+            </div>
+            <div class="mt-5">
+              <button class="w-full bg-orange-400 text-white py-2">Sign-Up</button>
+            </div>
+          </form>
         </div>
+    
       </div>
-    </div>
+    </div>    
+      
   </div>
 
 </body>
