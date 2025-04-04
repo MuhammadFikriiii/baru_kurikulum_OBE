ac46284cd845e438511db7e27e0882c678f64ec7
MuhammadFikriiii <tpublicml@gmail.com>
2025-04-04 10:07:37 +0800

    update seeder bk

--- database/seeders/BahanKajianSeeder.php
+++ database/seeders/BahanKajianSeeder.php
@@ -23,7 +23,27 @@ class BahanKajianSeeder extends Seeder
                 'knowledge_area'=> '-',
                 'max_bk'=> '60',
                 'min_bk'=>'40'
-            ]
+            ],
+            [
+                'kode_bk'=>'BK02',
+                'nama_bk'=> 'Algorithmic Foundations (AL)',
+                'deskripsi_bk'=> '-',
+                'referensi_bk'=> 'CSC2023',
+                'status_bk'=> 'elective',
+                'knowledge_area'=> '-',
+                'max_bk'=> '60',
+                'min_bk'=>'40'
+            ],
+            [
+                'kode_bk'=>'BK02',
+                'nama_bk'=> 'Architecture and Organization (AR)',
+                'deskripsi_bk'=> '-',
+                'referensi_bk'=> 'CSC2023',
+                'status_bk'=> 'elective',
+                'knowledge_area'=> '-',
+                'max_bk'=> '60',
+                'min_bk'=>'40'
+            ],
         ]);
     }
 }
