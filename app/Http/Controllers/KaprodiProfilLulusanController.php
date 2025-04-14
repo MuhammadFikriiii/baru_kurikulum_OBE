a22661e20329f5e6ded7b5dc76e08ec129a6ae99
MuhammadFikriiii <tpublicml@gmail.com>
2025-04-14 15:23:26 +0800

    added filter pl berdasarkan login prodi

--- app/Http/Controllers/KaprodiProfilLulusanController.php
+++ app/Http/Controllers/KaprodiProfilLulusanController.php
@@ -11,6 +11,10 @@ class KaprodiProfilLulusanController extends Controller
     public function index()
     {
         $user = Auth::guard('userprodi')->user();
+
+        if (!$user || !$user->kode_prodi) {
+            abort(404);
+        }
         $kodeProdi = $user->kode_prodi;
 
         $profillulusans = ProfilLulusan::where('kode_prodi', $kodeProdi)->with('prodi')->get();
