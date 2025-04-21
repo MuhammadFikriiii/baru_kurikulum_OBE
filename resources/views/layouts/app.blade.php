0dffa6ee4ffe94f866197f158a121d35f655447b
Bibie-art <muhammad08habibie@gmail.com>
2025-04-21 10:12:38 +0800

    perubahan layou app.blade php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
 <body class="bg-white min-h-screen flex">
 
     <!-- Sidebar -->
-    <aside id="sidebar" class="w-64 bg-[#201F31] text-white p-5 space-y-6 fixed top-0 left-0 h-full transform -translate-x-full md:translate-x-0 transition-transform duration-300 z-40">
+    <aside id="sidebar" class="w-64 bg-[#201F31] text-white p-5 space-y-6 fixed top-0 left-0 h-full overflow-y-auto transform -translate-x-full md:translate-x-0 transition-transform duration-300 z-40">
         <!-- Header -->
         <div class="flex items-center justify-between mb-4">
             <h2 class="text-xl font-bold">Dashboard Admin</h2>
