005addc3bbf6c104f41176be283b647dd65a80b5
MuhammadFikriiii <tpublicml@gmail.com>
2025-05-08 21:38:37 +0800

    benerin

--- app/Http/Controllers/AdminPemetaanCplBkController.php
+++ app/Http/Controllers/AdminPemetaanCplBkController.php
@@ -18,21 +18,25 @@ class AdminPemetaanCplBkController extends Controller
             ->join('capaian_profil_lulusans as cpl', 'cp.id_cpl', '=', 'cpl.id_cpl')
             ->join('profil_lulusans as pl', 'cp.id_pl', '=', 'pl.id_pl')
             ->select('cpl.*')
-            ->OrderBy('id_cpl', 'asc');
+            ->orderBy('id_cpl', 'asc');
         
         if ($kode_prodi && $kode_prodi !== 'all') {
             $query->where('pl.kode_prodi', $kode_prodi);
         }
-        
+
         $cpls = $query->get();
         $cplIds = $cpls->pluck('id_cpl');
-        
+
         $relasi = DB::table('cpl_bk')
             ->whereIn('id_cpl', $cplIds)
             ->get()
             ->groupBy('id_bk');
-        
-        $bks = BahanKajian::all();
+            
+        $bks = DB::table('bahan_kajians as bk')
+            ->join('cpl_bk as cb', 'bk.id_bk', '=', 'cb.id_bk')
+            ->whereIn('cb.id_cpl', $cplIds)
+            ->select('bk.*')
+            ->get();
         
         return view('admin.pemetaancplbk.index', compact('cpls', 'bks', 'relasi', 'kode_prodi', 'prodis'));
     }
