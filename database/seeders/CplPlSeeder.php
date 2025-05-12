f80a0ff130fff6004016344217222f195522632f
MuhammadFikriiii <tpublicml@gmail.com>
2025-05-12 11:19:40 +0800

    foxed bug

--- database/seeders/CplPlSeeder.php
+++ database/seeders/CplPlSeeder.php
@@ -15,13 +15,10 @@ class CplPlSeeder extends Seeder
     {
         $data = [
             ['id_cpl' => '1', 'id_pl' => 1],
-            ['id_cpl' => '1', 'id_pl' => 2],
             ['id_cpl' => '2', 'id_pl' => 1],
-            ['id_cpl' => '3', 'id_pl' => 2],
-            ['id_cpl' => '4', 'id_pl' => 3],
+            ['id_cpl' => '3', 'id_pl' => 1],
+            ['id_cpl' => '4', 'id_pl' => 2],
             ['id_cpl' => '5', 'id_pl' => 2],
-            ['id_cpl' => '5', 'id_pl' => 4],
-            ['id_cpl' => '6', 'id_pl' => 1],
             ['id_cpl' => '6', 'id_pl' => 2],
             ['id_cpl' => '7', 'id_pl' => 3],
             ['id_cpl' => '8', 'id_pl' => 4],
@@ -32,12 +29,9 @@ class CplPlSeeder extends Seeder
             ['id_cpl' => '12', 'id_pl' => 5],
             ['id_cpl' => '13', 'id_pl' => 6],
             ['id_cpl' => '14', 'id_pl' => 7],
-            ['id_cpl' => '15', 'id_pl' => 6],
             ['id_cpl' => '15', 'id_pl' => 8],
             ['id_cpl' => '16', 'id_pl' => 5],
-            ['id_cpl' => '16', 'id_pl' => 6],
             ['id_cpl' => '17', 'id_pl' => 7],
-            ['id_cpl' => '18', 'id_pl' => 8],
             ['id_cpl' => '18', 'id_pl' => 8],
         ];
         DB::table('cpl_pl')->insert($data);
