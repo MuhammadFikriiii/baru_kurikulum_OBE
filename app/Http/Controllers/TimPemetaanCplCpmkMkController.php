ca237a02ec3937fe41faefdc72bd56242e186f3d
MuhammadFikriiii <tpublicml@gmail.com>
2025-05-21 18:13:09 +0800

    pemenuhan cplcpmkmk tim

--- app/Http/Controllers/TimPemetaanCplCpmkMkController.php
+++ app/Http/Controllers/TimPemetaanCplCpmkMkController.php
@@ -36,7 +36,7 @@ class TimPemetaanCplCpmkMkController extends Controller
         $matrix = [];
         foreach ($data as $row) {
             $matrix[$row->kode_cpl]['deskripsi'] = $row->deskripsi_cpl;
-            $matrix[$row->kode_cpl]['cpmk'][$row->kode_cpmk]['deskripsi'] = $row->deskripsi_cpmk;
+            $matrix[$row->kode_cpl]['cpmk'][$row->kode_cpmk]['deskripsi'][] = $row->deskripsi_cpmk;
             $matrix[$row->kode_cpl]['cpmk'][$row->kode_cpmk]['mk'][] = $row->nama_mk;
         }
         return view('tim.pemetaancplcpmkmk.index', compact('matrix'));
@@ -46,14 +46,14 @@ class TimPemetaanCplCpmkMkController extends Controller
     {
         $kodeProdi = Auth::user()->kode_prodi;
 
-        $data = DB::table('cpl_cpmk')
-            ->join('capaian_pembelajaran_mata_kuliahs as cpmk', 'cpl_cpmk.id_cpmk', '=', 'cpmk.id_cpmk')
-            ->join('capaian_profil_lulusans as cpl', 'cpl_cpmk.id_cpl', '=', 'cpl.id_cpl')
+        $data = DB::table('capaian_profil_lulusans as cpl')
             ->Join('cpl_pl', 'cpl.id_cpl', '=', 'cpl_pl.id_cpl')
             ->join('profil_lulusans as pl', 'cpl_pl.id_pl', '=', 'pl.id_pl')
             ->join('prodis', 'pl.kode_prodi', '=', 'prodis.kode_prodi')
-            ->join('cpmk_mk', 'cpl_cpmk.id_cpmk', '=', 'cpmk_mk.id_cpmk')
-            ->join('mata_kuliahs as mk', 'cpmk_mk.kode_mk', '=', 'mk.kode_mk')
+            ->leftJoin('cpl_cpmk as cpl_cpmk', 'cpl_cpmk.id_cpl', '=', 'cpl.id_cpl')
+            ->leftJoin('capaian_pembelajaran_mata_kuliahs as cpmk', 'cpl_cpmk.id_cpmk', '=', 'cpmk.id_cpmk')
+            ->leftJoin('cpmk_mk', 'cpl_cpmk.id_cpmk', '=', 'cpmk_mk.id_cpmk')
+            ->leftJoin('mata_kuliahs as mk', 'cpmk_mk.kode_mk', '=', 'mk.kode_mk')
             ->where('prodis.kode_prodi', $kodeProdi)
             ->select(
                 'cpl.kode_cpl',
--- resources/views/layouts/tim/app.blade.php
+++ resources/views/layouts/tim/app.blade.php
@@ -202,6 +202,12 @@
                 </a>
             </li>
             <li>
+                <a href="{{ route('tim.pemetaancplcpmkmk.pemenuhancplcpmkmk') }}" class="flex items-center p-3 hover:bg-gray-700 rounded">
+                    <i class="fas fa-share-alt"></i>
+                    <span class="ml-2">Pemenuhan CPL - CPMK - MK</span>
+                </a>
+            </li>
+            <li>
                 <form action="{{ route('logout') }}" method="POST">
                     @csrf
                     <button type="submit" class="w-full flex items-center p-3 hover:bg-gray-700 rounded text-left">
