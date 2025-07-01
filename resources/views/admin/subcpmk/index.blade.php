afba3c780fa3be30ec4edbce28c471c8b2a6ac42
MuhammadFikriiii <tpublicml@gmail.com>
2025-07-01 16:16:21 +0800

    fix cpmk

--- resources/views/admin/subcpmk/index.blade.php
+++ resources/views/admin/subcpmk/index.blade.php
@@ -146,7 +146,7 @@
                                                 <path
                                                     d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                                             </svg>
-                                        </a>--}}
+                                        </a>
                                         <form action="{{ route('admin.subcpmk.destroy', $subcpmk->id_sub_cpmk) }}"
                                             method="POST">
                                             @csrf @method('DELETE')
@@ -160,7 +160,7 @@
                                                         clip-rule="evenodd" />
                                                 </svg>
                                             </button>
-                                        </form>
+                                        </form> --}}
                                     </div>
                                 </td>
                             </tr>
