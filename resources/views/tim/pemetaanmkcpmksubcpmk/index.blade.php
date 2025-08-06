24523aadd5ea6e735bf518ae48bc6592bf22dc49
Bibie-art <muhammad08habibie@gmail.com>
2025-08-07 03:09:34 +0800

    fix

--- resources/views/tim/pemetaanmkcpmksubcpmk/index.blade.php
+++ resources/views/tim/pemetaanmkcpmksubcpmk/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.tim.app')
 @section('content')
     <div class="bg-white p-4 md:p-6 lg:p-8 rounded-lg shadow-md mx-2 md:mx-0">
-        <h2 class="text-4xl font-extrabold text-center mb-4">Pemetaan MK - CPMK - SUBCPMK</h2>
-        <hr class="w-full border border-black mb-4">
+        <div class="text-center mb-8">
+            <h1 class="text-3xl font-bold text-gray-800">Pemetaan MK - CPMK - SUBCPMK</h1>
+            <hr class="border-t-2 md:border-t-4 border-black my-3 md:my-4 mx-auto">
+        </div>  
         <!-- Filter Tahun -->
         <select id="tahun" name="id_tahun"
             class="border border-black mb-4 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
